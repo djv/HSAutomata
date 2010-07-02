a8c2c1f4780d3110807bf9c34e3cb7e00c14dbef
dan <dan@dan-laptop.(none)>
2010-07-02 23:20:18 +0300

    revert removed optimization

--- run.hs
+++ run.hs
@@ -5,10 +5,11 @@ import Data.Set
 import System.Environment
 import Control.Monad
 import qualified Data.ByteString.Char8 as B
+--import Test.BenchPress
 
 main = do
     inpStr <- B.readFile =<< (liftM (!!1) $ getArgs)
     let inp = B.lines inpStr :: [B.ByteString]
     testSize <- liftM (read . (!!0)) $ getArgs :: IO Int
-    --putStrLn $ show $ stateSize $ buildTrie $ take testSize inp
-    putStrLn . show . stateSize . determinize . buildTrie $ take testSize inp
+    --putStrLn . show . stateSize . determinize . buildTrie $ take testSize inp
+    (stateSize . determinize . buildTrie $ take testSize inp) `seq` return ()
