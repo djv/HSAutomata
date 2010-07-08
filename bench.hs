0dcf3d4f9d91e46401e4657eb162edbe977c0c4d
dan <dan@dan-laptop.(none)>
2010-07-09 00:39:22 +0300

    added interactive mode

--- bench.hs
+++ bench.hs
@@ -8,9 +8,9 @@ import Data.Time.Clock
 import Control.Monad
 import qualified Data.ByteString.Char8 as B
 import List
---import Test.BenchPress
+import Test.BenchPress
 
-main = --bench 5 $
+main = bench 5 $
     do
     args <- getArgs
     inpStr <- B.readFile $ args !! 0
@@ -18,19 +18,4 @@ main = --bench 5 $
     let testSize = read $ args !! 1 :: Int
     let dfa = determinize . buildTrie $ take testSize inp
     --putStrLn . show . stateSizeReal . determinize . buildTrie $ take testSize inp
-    loop dfa
-
-loop dfa = do
-    putStr "> "
-    hFlush stdout
-    inp <- getLine
-    case inp of
-         "q" -> return ()
-         otherwise -> do 
-            start <- getCurrentTime
-            print . length {-. nub . sort-} $ matchRegex dfa (parseRegex inp)
-            finish <- getCurrentTime
-            print $ diffUTCTime finish start
-            loop dfa
-
-anySym = Sum [Symbol 'a', Symbol 'b', Symbol 'c']
+    print . length {-. nub . sort-} $ matchRegex dfa (parseRegex "2*.a.b.c.2*")
--- prof.hs
+++ prof.hs
@@ -8,29 +8,12 @@ import Data.Time.Clock
 import Control.Monad
 import qualified Data.ByteString.Char8 as B
 import List
---import Test.BenchPress
 
-main = --bench 5 $
-    do
+main = do
     args <- getArgs
     inpStr <- B.readFile $ args !! 0
     let inp = B.lines inpStr :: [B.ByteString]
     let testSize = read $ args !! 1 :: Int
     let dfa = determinize . buildTrie $ take testSize inp
     --putStrLn . show . stateSizeReal . determinize . buildTrie $ take testSize inp
-    loop dfa
-
-loop dfa = do
-    putStr "> "
-    hFlush stdout
-    inp <- getLine
-    case inp of
-         "q" -> return ()
-         otherwise -> do 
-            start <- getCurrentTime
-            print . length {-. nub . sort-} $ matchRegex dfa (parseRegex inp)
-            finish <- getCurrentTime
-            print $ diffUTCTime finish start
-            loop dfa
-
-anySym = Sum [Symbol 'a', Symbol 'b', Symbol 'c']
+    print . length {-. nub . sort-} $ matchRegex dfa (parseRegex "2*.a.b.c.2*")
